cc4f14481695ae1961ea5e78c25bb8bc35de0121
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-14 17:46:23 +0800

    update here

--- BasicsOfpython/Loop.py
+++ BasicsOfpython/Loop.py
@@ -4,10 +4,11 @@
 # WHILE LOOP
 
 
-# def is define 
+# def is define this is function 
 
 def loop() :
     
+    # In english for each num in the range from 1 to 9, print that num
     for num in range(1, 10) :
         print(num)
 
