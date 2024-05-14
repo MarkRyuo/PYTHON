bbf2132f26fa353f38af1dda8f95e4376016efc6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-14 17:48:00 +0800

    update here

--- BasicsOfpython/Loop.py
+++ BasicsOfpython/Loop.py
@@ -9,6 +9,7 @@
 def loop() :
     
     # In english for each num in the range from 1 to 9, print that num
+    # i-isa isahin na ilalagay sa num yung mga number start in num 1, num 2 to num 9 
     for num in range(1, 10) :
         print(num)
 
