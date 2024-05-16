92aab20654b08a9452d45f21a7c40e230cb88905
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-16 22:19:28 +0800

    updat eherE

--- BasicsOfpython/Loops/L1.py
+++ BasicsOfpython/Loops/L1.py
@@ -7,15 +7,14 @@ people = [
     "Mark"
 ]
 
+for i in people :
+        print(i)
 
 while True :
-
-    for i in people :
-        print(i)
     
     _name = input("Who is your Favorite?: ")
 
-    if _name == people :
+    if _name == people[] :
         print(_name)
     else :
         print("Who is that?")
