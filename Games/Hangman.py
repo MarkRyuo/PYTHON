36be1b14fd6196013d6d182319448fd3fa08caa6
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-17 17:26:21 +0800

    updat eherE

--- Games/Hangman.py
+++ Games/Hangman.py
@@ -3,4 +3,4 @@
 # randomly choose a word in the list 
 # ask the user to guess a letter 
 # bonus make the program take the input from the user and make it lowercase 
-#
+# check if the letter is in the word 
