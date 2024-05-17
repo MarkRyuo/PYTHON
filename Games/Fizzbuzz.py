509cea30fe7d36aa711e585e4210f9cf3c936ac2
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-17 16:38:51 +0800

    updat eherE

--- Games/Fizzbuzz.py
+++ Games/Fizzbuzz.py
@@ -3,7 +3,8 @@
 
 def fizzbuzz() :
 
-    for i in range(1, 100) :
+    for num in range(1, 100) :
 
-        if i % 3 == 0 and i % 5 == 0 :
-             
+        if num % 3 == 0 and num % 5 == 0 :
+            print("fizzbuzz")
+        elif i  
