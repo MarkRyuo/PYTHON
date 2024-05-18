30dcb9f290980b2f4f6a682147cba780895a6295
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-18 18:01:06 +0800

    update herE

--- Games/Hangman.py
+++ Games/Hangman.py
@@ -32,3 +32,12 @@
     
 
 # main()    
+
+
+# create a greeting 
+# create a word list 
+# randomly choose a word in the list 
+# ask the user to guess a letter 
+# bonus make the program take the input from the user and make it lowercase 
+# check if the letter is in the word 
+
