3caaa369eb2827fb0dcc52ed873f36170363506b
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-18 18:00:44 +0800

    updta eherE

--- Games/Hangman.py
+++ Games/Hangman.py
@@ -6,29 +6,29 @@
 # check if the letter is in the word 
 
 
-list_of_word = [
-        "Spaces",
-        "Python",
-        "Golive",
-        "Prettier"
-    ]
+# list_of_word = [
+#         "Spaces",
+#         "Python",
+#         "Golive",
+#         "Prettier"
+#     ]
 
-greet = input("Enter your name: ") 
+# greet = input("Enter your name: ") 
 
-def Greeting(greet) :
+# def Greeting(greet) :
     
-    print(f"Konichiwa {greet}")
+#     print(f"Konichiwa {greet}")
 
-def _List(list_of_word) :
+# def _List(list_of_word) :
 
-    for i in list_of_word :
-        print(i) 
+#     for i in list_of_word :
+#         print(i) 
 
-def main() :
+# def main() :
 
-    Greeting(greet)
-    print("This is the list of words: ")
-    _List(list_of_word)
+#     Greeting(greet)
+#     print("This is the list of words: ")
+#     _List(list_of_word)
     
 
-main()    
+# main()    
