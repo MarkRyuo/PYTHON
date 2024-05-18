9e0d77ad153a05c63282403767bbf86fb644a109
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-18 18:19:44 +0800

    update herE

--- Games/Hangman.py
+++ Games/Hangman.py
@@ -49,7 +49,7 @@ def greetings() :
     while True :
         question_1 = input("Are you playing (y/n): ")
         if question_1 :
-            question_1 = question_1.lower(question_1)
+            question_1.lower()
             if question_1 == "y" :
                 break 
             elif question_1 == "n":
