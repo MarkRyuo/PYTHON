80a6b78133dfbb24b144dc91a7f2a6d31feb4fa2
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-20 17:06:15 +0800

    update herE

--- BasicsOfpython/Random.py
+++ BasicsOfpython/Random.py
@@ -12,5 +12,5 @@ wordsOf = random.choice(words) # Kukuha siya ng random word sa words list
 display_word = [] # eto ang mag sisilbing lalagyan 
 
 for letter in wordsOf : # for loop 
-    display_word += " " 
+    display_word += "_" 
 print(display_word)
