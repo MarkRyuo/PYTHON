354db05b7d4b2a459b5cb0ba8f656ddc5d4e2b4c
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-20 17:09:34 +0800

    update herE

--- Games/Hangman.py
+++ Games/Hangman.py
@@ -42,6 +42,58 @@
 # check if the letter is in the word 
 
 
+# def greetings() :
+
+#     print("Welcome to hangman")
+
+#     while True :
+#         question_1 = input("Are you playing (y/n): ").lower()
+#         if question_1 == "y" :
+#             break 
+#         elif question_1 == "n":
+#             question_2 = input("Are you sure (y/n): ")
+#             if question_2 == "y":
+#                 print("Okay thank you for coming")
+#                 break
+#             elif question_2 == "n":
+#                 continue
+#             else:
+#                 print(f"{question_1} not in choice")
+#         else :
+#             print("Please enter y or n")
+        
+    
+#     return question_1
+
+
+# def List_Of():
+
+#     list_of_word = [
+#         "Hacker",
+#         "User",
+#         "Admin"
+#     ]
+#     return list_of_word
+
+
+
+# def main() :
+
+#     decision = greetings() 
+#     if decision :
+#         words = List_Of()
+#         question_3 = input("Guess the word (This is a users of the Internet): ")
+#         if question_3 in words :
+#             print("You Win")
+#             print(f"The {question_3} in here in the list: {words }")
+#         else :
+#             print("You Lose")
+
+# main()
+
+
+
+
 def greetings() :
 
     print("Welcome to hangman")
