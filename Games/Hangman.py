373422fb09ff6fc120d684b8a6ec1f819f910286
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-23 18:33:24 +0800

    update herE

--- Games/Hangman.py
+++ Games/Hangman.py
@@ -167,7 +167,7 @@ main()
 
 # Todo create a greeting 
 # Todo create a word list 
-# randomly choose a word in the list 
+# Todo randomly choose a word in the list 
 # ask the user to guess a letter 
 # bonus make the program take the input from the user and make it lowercase 
 # check if the letter is in the word 
