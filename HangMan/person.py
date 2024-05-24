82f0de6b48c82fe3dc180eb533653ee502c546f1
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-24 17:06:36 +0800

    update here

--- HangMan/person.py
+++ HangMan/person.py
@@ -6,8 +6,8 @@ class Greeting():
     def __init__(self, name) :
         self.name = name 
     
-    def speak1(self) :
-        
+    def speak1() :
+        print("Welcome to Hangman")
 
     def speak2(self) :
         print(
