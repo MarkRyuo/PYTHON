b80ce212eec69fd1a79375da31d959a998b8f02a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-24 16:50:52 +0800

    update herE:

---
+++ HangMan/main.py
@@ -0,0 +1,15 @@
+# Todo create a greeting 
+# Todo create a word list 
+# Todo randomly choose a word in the list 
+# Todo ask the user to guess a letter 
+# Todo bonus make the program take the input from the user and make it lowercase 
+# Todo check if the letter is in the word 
+
+
+# Todo Loop through each of the letters in the chosen word 
+# Todo if the letter is in the word replace the "_" with the letter 
+# Todo it should look like this "_", "d", "_", "i", "_"
+
+
+# Todo Use a while loop so your game keeps going 
+# Todo until the word has been guessed 
