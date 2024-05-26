95fa0f9bfe0b0d0fed0e06b617fc54af994064bc
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-26 12:21:43 +0800

    update here

--- HangMan/main.py
+++ HangMan/main.py
@@ -37,7 +37,8 @@ def Greeting():
             # pass # Todo Gumawa ng condition kapag number lang nilagay ay huwag ito papasukin 
         elif ask_Username in list_of_ban :
             print(f"Your ban here user {ask_Username}")
-            _username = False
+            # _username = False
+            break
         elif ask_Username :
             break
         else : 
