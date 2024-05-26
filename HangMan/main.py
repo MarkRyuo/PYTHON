3847e0fc5c174cc9b29d35577e6b2e48dbb9aab9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-26 12:39:37 +0800

    update here

--- HangMan/main.py
+++ HangMan/main.py
@@ -38,7 +38,6 @@ def Greeting():
 
         if ask_Username.isdigit():
             print("Number? Unsupported Username")
-            # continue
             # Todo Gumawa ng condition kapag number lang nilagay ay huwag ito papasukin 
         elif ask_Username in list_of_ban :
             print(f"Your ban here user {ask_Username}")
