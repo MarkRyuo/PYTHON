22b342d1858b9ff1c20400f5b9bb754eebb7cf33
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-28 17:44:28 +0800

    update here

--- BasicsOfpython/Loops/L2.py
+++ BasicsOfpython/Loops/L2.py
@@ -15,5 +15,5 @@ _names = random.choice(list_of_name)
 empty = []
 
 for letter in _names : # * Ang laman ng letter ay ang random letter sa list_of_name
-    empty += letter
-print
+    empty += letter # * 
+print(empty)
