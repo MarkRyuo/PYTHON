6668fcd81fe863147ba6529d2d5fa3ab37db38f8
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-29 17:27:15 +0800

    update here

--- BasicsOfpython/Loops/L2.py
+++ BasicsOfpython/Loops/L2.py
@@ -19,8 +19,8 @@ list_of_name = [
 _names = random.choice(list_of_name) 
 empty_list = []
 
-for letter in _names : # * Ang laman ng letter ay ang random letter sa list_of_name
-    empty_list += letter # * Ang empty ay increamented sa letter magkakaroon ng laman ang empty list  
+# for letter in _names : # * Ang laman ng letter ay ang random letter sa list_of_name
+#     empty_list += letter # * Ang empty ay increamented sa letter magkakaroon ng laman ang empty list  
 # print(empty_list) 
 
 this_is_dict = {
