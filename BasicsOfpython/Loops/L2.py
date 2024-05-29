f915034617125e402dfb0a23d79c732cc972d182
MarkRyuo <malupajhonmark3035@gmail.com>
2024-05-29 17:50:03 +0800

    update here

--- BasicsOfpython/Loops/L2.py
+++ BasicsOfpython/Loops/L2.py
@@ -54,7 +54,7 @@ def List_() :
 
     while list_ :
 
-        for names in list_of_name :
+        for names in names_ :
             if names == "Riyuo" :
                 list_ = False
             else : 
