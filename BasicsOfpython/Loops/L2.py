177976d23e138bf11d8a5bc330bf348b1750f26d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-01 17:54:58 +0800

    :updat eherE

--- BasicsOfpython/Loops/L2.py
+++ BasicsOfpython/Loops/L2.py
@@ -47,22 +47,3 @@ def print_dict() :
 # print_dict()
 
 
-# * Scope (global, local)
-# * Function(def) - Nested Function 
-
-
-global_variable = "Hello World Global Variable" # * Global variable 
-
-def Outer() :
-
-    global_variable = "Hello local variable"
-    print(global_variable)
-    def Inner() :
-
-        global_variable = "Hello Inner variable in Nested Function"
-        print(global_variable)
-    Inner()
-
-Outer()
-
-print(global_variable)
