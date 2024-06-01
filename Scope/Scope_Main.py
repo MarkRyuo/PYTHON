860d60a888b09d959cf315a4a93c92c18e23191f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-01 17:55:12 +0800

    update herE

---
+++ Scope/Scope_Main.py
@@ -0,0 +1,23 @@
+
+
+
+
+# * Scope (global, local)
+# * Function(def) - Nested Function 
+
+
+global_variable = "Hello World Global Variable" # * Global variable 
+
+def Outer() :
+
+    global_variable = "Hello local variable"
+    print(global_variable)
+    def Inner() :
+
+        global_variable = "Hello Inner variable in Nested Function"
+        print(global_variable)
+    Inner()
+
+Outer()
+
+print(global_variable)
