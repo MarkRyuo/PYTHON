0e7358d99bc3885d010b9db512e2409b0ea4951d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-03 17:43:34 +0800

    update herE

--- Scope/ScopePractice/Scope1.py
+++ Scope/ScopePractice/Scope1.py
@@ -5,15 +5,15 @@ variable_age = 21 # * Global Variable
 
 def Age() :
 
-    variable_age = 19 # * Local Variable  
+    # variable_age = 19 # * Local Variable  
     
-    QUESTION1 = input("What is your age?: ")
-    QUESTION1 = int(QUESTION1)
+    # QUESTION1 = input("What is your age?: ")
+    # QUESTION1 = int(QUESTION1)
 
-    if QUESTION1 == variable_age :
-        print("Your age is not bad!")
-    else :
-        print(f"{QUESTION1} is bad {QUESTION1} is good")
+    # if QUESTION1 == variable_age :
+    #     print("Your age is not bad!")
+    # else :
+    #     print(f"{QUESTION1} is bad {QUESTION1} is good")
         
     def Inner_age() :
 
