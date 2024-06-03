13a3fce0f8daeaeaa6c1221fa66cb4c4a08c441d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-03 17:20:54 +0800

    update herE

--- Scope/ScopePractice/Scope1.py
+++ Scope/ScopePractice/Scope1.py
@@ -7,8 +7,8 @@ def Age() :
 
     variable_age = 19 # * Local Variable  
     
-    question1 = input("What is your age?: ")
-    question1 = int(question1)
+    QUESTION1 = input("What is your age?: ")
+    QUESTION1 = int(question1)
 
     if question1 == variable_age :
         print("Your age is not bad!")
