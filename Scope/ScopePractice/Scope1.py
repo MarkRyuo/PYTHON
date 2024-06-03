884b4eb88ded54a7ff49c4d1a08cec7bf89f7cbf
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-03 17:21:15 +0800

    update herE

--- Scope/ScopePractice/Scope1.py
+++ Scope/ScopePractice/Scope1.py
@@ -10,7 +10,7 @@ def Age() :
     QUESTION1 = input("What is your age?: ")
     QUESTION1 = int(QUESTION1)
 
-    if question1 == variable_age :
+    if QUESTION1 == variable_age :
         print("Your age is not bad!")
     else :
         print(f"{question1} is bad {variable_age} is good")
