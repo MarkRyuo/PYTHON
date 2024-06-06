001a16aba289c3db9103897965c1f49f5101189f
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-06 17:01:47 +0800

    update here

--- Scope/ScopePractice/Scope2.py
+++ Scope/ScopePractice/Scope2.py
@@ -51,8 +51,7 @@ def Ages_1() :
         for i in ages :
             if i == int(Q1) :
                 print("UNLOCK🔓") 
-                age = False
-                break
+                return Q1
         else :
             print("Try again")
 
