223c5e91b07de48d5294a33ff346f920868dfe49
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-06 17:06:54 +0800

    update here

--- Scope/ScopePractice/Scope2.py
+++ Scope/ScopePractice/Scope2.py
@@ -60,7 +60,7 @@ def Ages_1() :
             else :
                 print("Attempt Block")
     
-        return None # 
+        return None # ! None Understanding 
     # Todo create a 2 attempt block 
 
 
