10c3bac0395bcc4dff8577e3e22250a03b643886
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-07 17:48:44 +0800

    update herE

--- Scope/ScopePractice/Scope3.py
+++ Scope/ScopePractice/Scope3.py
@@ -8,12 +8,13 @@ variable = "This is Global Variable"
 def Variable() :
 
     variable = "This is a Local Variable"
+    print(variable)
 
     def InnerVariable() :
 
         variable = "Local varible in a Nested function"
         print(variable)
-        
+
     InnerVariable()
     
 
