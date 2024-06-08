55b1f7e28e65a466efe272e6e9ccf35737bab9bd
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-08 16:47:16 +0800

    update herE

--- BasicsOfpython/Loops/L3.py
+++ BasicsOfpython/Loops/L3.py
@@ -12,5 +12,5 @@
 # * When iterating over a dictionary, you can use methods such as .keys(), .values(), or .items() to get the keys, values, or key-value pairs respectively.
 
 person = {'name': 'Alice', 'age': 25}
-for key in person:
-    print(key, person[items])
+for items in person:
+    print(items, person[items])
