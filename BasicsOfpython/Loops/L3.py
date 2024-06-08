5753a9e4fbcc4eaea584bb88bbfb5c5db50250c1
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-08 16:51:16 +0800

    update herE

--- BasicsOfpython/Loops/L3.py
+++ BasicsOfpython/Loops/L3.py
@@ -12,6 +12,7 @@
 # * When iterating over a dictionary, you can use methods such as .keys(), .values(), or .items() to get the keys, values, or key-value pairs respectively.
 
 # * DICT 
+
 person = {'name': 'Alice', 'age': 25}
 for items in person:
     print(items, person[items])
