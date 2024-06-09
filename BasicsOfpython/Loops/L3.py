844fadbefc449718db39dfe5db958c7ae421df64
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-09 17:33:52 +0800

    update herE

--- BasicsOfpython/Loops/L3.py
+++ BasicsOfpython/Loops/L3.py
@@ -96,7 +96,7 @@ def Log_in() :
 
     username = input("Enter your name: ") # * Enter username
 
-    if username == data.keys :
+    if username == data["name"] :
         print("Hello")
     else :
         print("bye")
